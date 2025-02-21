96c19bc3c2b632eb4e5a4b3439b199e3914872f3
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-21 15:45:20 +0500

    Create index.js

---
+++ JavaScript/Lessons/lesson-04-loops/index.js
@@ -0,0 +1,39 @@
+// let a = 0;
+
+// For Loop
+// for(let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Areeb",
+//     role: "Developer",
+//     age:22
+// }
+
+// // For in Loop
+// for (const key in obj) {    
+//     const element = obj[key];
+//     console.log(key , element)
+// }
+
+// // For of Loop
+// for (const char of "Hello world") {
+//     console.log(char)
+// }
+
+
+// While Loop
+let i = 10;
+
+// while (i < 6) {
+//     console.log(i)
+//     i++;
+// }
+
+
+// Do While Loop
+do {
+    console.log(i)
+    i++
+}while(i < 6)
