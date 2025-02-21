ac209e2ec2609fc48b67823edc9dbe2f2ab9aaa3
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-21 15:59:50 +0500

    Create index.js

---
+++ JavaScript/Lessons/lesson-05-functions/index.js
@@ -0,0 +1,35 @@
+// function nice(name){
+//     console.log("Hey " + name + " you are nice");
+//     console.log("Hey " + name + " you are good too");
+//     console.log("Hey " + name + " your T-shirt is also nice");
+//     console.log("Hey " + name + " your style of talking is awesome");
+// }
+
+// nice("Areeb");
+// nice("Kamran");
+// nice("Sara");
+// nice("Zoha");
+
+// function sum(a,b) {
+//     // console.log(a+b)
+//     return a + b 
+// }
+
+// result1 = sum(3,5)
+// result2 = sum(4,6)
+// result3 = sum(2,23)
+
+// console.log("The Sum of These Numbers is "+ result1)
+// console.log("The Sum of These Numbers is "+ result2)
+// console.log("The Sum of These Numbers is "+ result3)
+
+
+// Arrow Functions
+
+const func1 = (x) => {
+    console.log("Arrow Function value is " + x)
+}
+
+func1(23);
+func1(2);
+func1(55);
