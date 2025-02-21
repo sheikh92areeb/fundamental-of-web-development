469fb15f06fce501315e1569bc89207036a44d3d
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-21 20:09:07 +0500

    Create index.js

---
+++ JavaScript/Projects/project-01/index.js
@@ -0,0 +1,49 @@
+let m = 2;
+let n = 4;
+let o = "+";
+// let o = "-";
+// let o = "*";
+// let o = "/";
+
+function faulty(m,n,o) {
+    if (o == "+") {
+        return m - n
+    }else if (o == "-") {
+        return m / n
+    }else if (o == "*"){
+        return m + n
+    }else if (o == "/") {
+        return m * n
+    }
+}
+
+function calculate(m,n,o) {
+    if (o == "+") {
+        return m + n
+    }else if (o == "-") {
+        return m - n
+    }else if (o == "*"){
+        return m * n
+    }else if (o == "/") {
+        return m / n
+    }
+}
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+if (random <= 10) {
+    result = faulty(m,n,o);
+    console.log(result);
+}else{
+    result = calculate(m,n,o);
+    console.log(result);
+}
+
+
+// const faultyOps = { "+": "-", "-": "/", "*": "+", "/": "**" };
+// const correctOps = { "+": "+", "-": "-", "*": "*", "/": "/" };
+
+
+// let chosenOp = random <= 10 ? faultyOps[o] : correctOps[o];
+// result = eval(`${m} ${chosenOp} ${n}`);
+// console.log(result);
