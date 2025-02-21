13735e77c04a3f71320871f25d5c62b76c1e4714
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-22 00:42:33 +0500

    Create index.js

---
+++ JavaScript/Lessons/lesson-06-string/index.js
@@ -0,0 +1,23 @@
+let a = "Areeb";
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]); it throw the error
+
+console.log(a.length);
+
+let my_name = "Areeb";
+let friend = "Ahmad";
+
+console.log(`I am ${my_name} and ${friend} is my friend.`)
+
+console.log(my_name.toUpperCase())
+console.log(my_name.toLowerCase())
+
+console.log(my_name.slice(1,4))
+console.log(my_name.replace("ee","ii"))
+console.log(my_name.concat(friend))
