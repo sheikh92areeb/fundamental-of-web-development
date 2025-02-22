d00295a9e3389bfae4f6a7ff52a07136161a836a
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-22 14:59:26 +0500

    Create index.js

---
+++ JavaScript/Projects/project-02/index.js
@@ -0,0 +1,38 @@
+let random1 = Math.floor(Math.random() * 3) + 1;
+let random2 = Math.floor(Math.random() * 3) + 1;
+let random3 = Math.floor(Math.random() * 3) + 1;
+
+let adjective1 = "Crazy";
+let adjective2 = "Amazing";
+let adjective3 = "Fire";
+
+let shop1 = "Engine";
+let shop2 = "Foods";
+let shop3 = "Garments";
+
+let word1 = "Bros";
+let word2 = "Limited";
+let word3 = "Hub";
+
+let selectAdj = "";
+let selectShop = "";
+let selectWord = "";
+
+(random1 === 1)?selectAdj = adjective1:
+(random1 === 2)?selectAdj = adjective2:
+(random1 === 3)?selectAdj = adjective3:
+selectAdj = null;
+
+(random2 === 1)?selectShop = shop1:
+(random2 === 2)?selectShop = shop2:
+(random2 === 3)?selectShop = shop3:
+selectShop = null;
+
+(random3 === 1)?selectWord = word1:
+(random3 === 2)?selectWord = word2:
+(random3 === 3)?selectWord = word3:
+selectWord = null;
+
+let Business_name = `${selectAdj}  ${selectShop}  ${selectWord}`;
+
+console.log(Business_name);
