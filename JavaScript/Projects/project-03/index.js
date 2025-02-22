4cac5f8c023e05a0828ee4eca8296c37f4a30f27
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-22 23:59:11 +0500

    Update index.js

--- JavaScript/Projects/project-03/index.js
+++ JavaScript/Projects/project-03/index.js
@@ -15,3 +15,25 @@
 // let factorial = arr.reduce(mul);
 
 // console.log(factorial);
+
+
+// let n = 11;
+// let factorial = 1;
+
+// for (let i = n; i > 1; i--) {
+//     factorial *= i;
+// }
+
+// console.log(`The factorial is ${factorial}`);
+
+
+// function mul(a, b) {
+//     return a * b;
+// }
+
+// let n = 11;
+// let arr = n > 0 ? Array.from({ length: n }, (_, i) => i + 1) : [1];
+
+// let factorial = arr.reduce(mul);
+
+// console.log(`The factorial is ${factorial}`);
