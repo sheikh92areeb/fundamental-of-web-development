34dd70e57694bc07b54731056ddcc413c08f67b9
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-25 00:57:51 +0500

    update

---
+++ JavaScript/Lessons/lesson-17-advance-JS/script.js
@@ -0,0 +1,38 @@
+// async function sleep () {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(45)
+//         }, 1000)
+//     })
+// }
+
+// (async function main() {
+//     let a = await sleep()
+//     console.log(a)
+//     let b = await sleep()
+//     console.log(b)
+// })()
+
+
+// let [x,y, ...rest] = [1,5,7,11,13];
+
+// console.log(x,y, rest)
+
+// let obj = {a:1, b:2, c:3}
+
+// let {a,b} = obj
+
+// console.log(a,b)
+
+// function sum(a,b,c) {
+//     return a + b + c;
+// }
+
+// let arr = [1,5,7]
+
+// console.log(sum(arr[0],arr[1],arr[2]))
+// console.log(sum(...arr))
+
+// let obj = {...arr}
+
+// console.log(obj)
