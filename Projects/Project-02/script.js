c7bd19f15c10d93a277c164c3dde732a7a8f5840
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-03 16:23:52 +0500

    update

--- Projects/Project-02/script.js
+++ Projects/Project-02/script.js
@@ -98,5 +98,13 @@ async function main() {
         document.querySelector(".circle").style.left = percent + "%";
         currentSong.currentTime = ((currentSong.duration) * percent) / 100
     })
+
+    document.querySelector("#hamburger").addEventListener("click", ()=> {
+        document.querySelector(".left").style.left = "0";
+    })
+
+    document.querySelector("#close").addEventListener("click", ()=> {
+        document.querySelector(".left").style.left = "-100%";
+    })
 }
 main();
